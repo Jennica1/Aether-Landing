ee6f8b648778ffbe15835133b8b384632461f090
Jennica1 <jsask1@my.bcit.ca>
2024-11-21 19:45:41 -0800

    first commit

---
+++ components/atoms/CTA.js
@@ -0,0 +1,11 @@
+export default function CTA() {
+  return (
+    <>
+      <div>
+        <p>Ready to get started?</p>
+        <p>test</p>
+        <button>Get Started</button>
+      </div>
+    </>
+  );
+}
---
+++ components/atoms/CardInfo.js
@@ -0,0 +1,37 @@
+import "./cardInfo.css";
+
+export default function CardInfo() {
+  return (
+    <>
+      <div className="container">
+        <div className="box" style={{ backgroundColor: "black" }}>
+          <header>
+            <p>Ease of Access:</p>
+          </header>
+          <p>Everything in one place.  Access everything you need all in one app. </p>
+        </div>
+
+        <div className="box" style={{ backgroundColor: "black" }}>
+          <header>
+            <p>Organization:</p>
+          </header>
+          <p>Everything in one place.  Access everything you need all in one app. </p>
+        </div>
+
+        <div className="box" style={{ backgroundColor: "black" }}>
+          <header>
+            <p>Autofill Feature:</p>
+          </header>
+          <p>With our AI auto filling feature, filling out forms have never been easier. </p>
+        </div>
+
+        <div className="box" style={{ backgroundColor: "black" }}>
+          <header>
+            <p>Simplifcation Made Simple:</p>
+          </header>
+          <p>Difficulty understanding is a thing of the past now that Aether is here to breakdown  document lingo in a easy-to-understand way. </p>
+        </div>
+      </div>
+    </>
+  );
+}

9678d3c89532cfa82512c335f90b90a0afb5bc07
parnelltse <parnell.tse@mgail.com>
2024-11-27 13:28:02 -0800

    adding work with us

--- components/molecules/Header.js
+++ components/molecules/Header.js
@@ -1,24 +1,29 @@
 import Image from "next/image";
-import './header.style.css'; 
+import "./header.style.css";
 
 export default function Header() {
   return (
     <>
       <div className="header">
-        <Image className="logo" src="/AetherLogo.png" width={100} height={100} alt="" />
+        <Image
+          className="logo"
+          src="/AetherLogo.png"
+          width={100}
+          height={100}
+          alt=""
+        />
 
         <div className="nav-links">
-        <p>Products</p>
-        <p>Developers</p>
-        <p>Blog</p>
+          <p>Products</p>
+          <p>Work With Us</p>
+          <p>Blog</p>
         </div>
 
         <div className="btn-cont">
-            <p>Login</p>
-            <button>Get Started</button>
+          <p>Login</p>
+          <button>Get Started</button>
         </div>
       </div>
     </>
   );
 }
-

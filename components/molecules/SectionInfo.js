c6e53989cae747d242b86a2b6662063fda524e4a
parnelltse <parnell.tse@mgail.com>
2024-12-02 14:20:56 -0800

    update image

--- components/molecules/SectionInfo.js
+++ components/molecules/SectionInfo.js
@@ -1,21 +1,21 @@
-import Image from 'next/image';  // Import the Next.js Image component
+import Image from "next/image"; // Import the Next.js Image component
 import CardInfo from "../atoms/CardInfo";
-import styles from "./SectionInfo.module.css"; 
+import styles from "./SectionInfo.module.css";
 
 export default function Info() {
   return (
-    <div className={styles.cont}> 
-      <p className={styles.introText}>How we make your life easier:</p> 
+    <div className={styles.cont}>
+      <p className={styles.introText}>How we make your life easier:</p>
       <div>
         <CardInfo />
       </div>
       <div className={styles.imageContainer}>
-        <Image 
-          src="/section1.png"      
-          alt="Section Illustration" 
-          layout="responsive"    
-          width={100}            
-          height={30}             
+        <Image
+          src="/section.png"
+          alt="Section Illustration"
+          layout="responsive"
+          width={100}
+          height={30}
         />
       </div>
     </div>

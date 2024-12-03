6c3e05566e127dd77a3a23a93a87c0c6a837d34a
parnelltse <parnell.tse@mgail.com>
2024-12-03 14:24:55 -0800

    padding and icons

--- components/atoms/CardInfo.js
+++ components/atoms/CardInfo.js
@@ -4,7 +4,11 @@ export default function CardInfo() {
   return (
     <div className="image-container">
       <div className="image-box">
-        <img src="accessibility.png" alt="Ease of Access" />
+        <img
+          className="card-image"
+          src="accessibility.png"
+          alt="Ease of Access"
+        />
         <p className="caption">
           Ease of Access: Everything in one place. Access everything you need
           all in one app.
@@ -12,7 +16,7 @@ export default function CardInfo() {
       </div>
 
       <div className="image-box">
-        <img src="checklist.png" alt="Organization" />
+        <img className="card-image" src="checklist.png" alt="Organization" />
         <p className="caption">
           Organization: Everything in one place. Access everything you need all
           in one app.
@@ -20,7 +24,7 @@ export default function CardInfo() {
       </div>
 
       <div className="image-box">
-        <img src="autofill.png" alt="Autofill Feature" />
+        <img className="card-image" src="autofill.png" alt="Autofill Feature" />
         <p className="caption">
           Autofill Feature: With our AI auto-filling feature, filling out forms
           has never been easier.

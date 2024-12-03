50b25e7b70ad1162117e90a8432162ea02b49207
parnelltse <parnell.tse@mgail.com>
2024-12-03 15:23:23 -0800

    review fix

--- components/molecules/Reviews.js
+++ components/molecules/Reviews.js
@@ -1,41 +1,24 @@
-import Image from "next/image"
-import './reviews.styles.css'
+import Image from "next/image";
+import "./reviews.styles.css";
 
 export default function Reviews() {
-    return(
-        <>
-        <div className="review-container">
-            <br/>
-            <p className="join">Join other happy caretakers who trust Aether</p>
-
-            <div className="proof">
-                <div className="people">
-                    <Image src='/CK.png' alt='people image' width={100} height={85} className="image"/>
-                    <p className="name">Comfort Keeper</p>
-                </div>
-                <div className="people">
-                    <Image src='/HI.png' alt='people image' width={100} height={85} className="image"/>
-                    <p className="name">Home Instead</p>
-                </div>
-                <div className="people">
-                    <Image src='/hannah.jpeg' alt='people image' width={100} height={85} className="image"/>
-                    <p className="name">Hannah Starling</p>
-                </div>
-                <div className="people">
-                    <Image src='/CC.png' alt='people image' width={100} height={85} className="image"/>
-                    <p className="name">Care and Company</p>
-                </div>
-                <div className="people">
-                    <Image src='/megan.jpeg' alt='people image' width={100} height={85} className="image"/>
-                    <p className="name">Megan Wilson</p>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="review-container">
+        <div>Our Core Values</div>
+        <div>
+          <p>Clarify</p>
+          <p>Text for Clarify</p>
         </div>
-        </>
-    )
+        <div>
+          <p>Summarize</p>
+          <p>Summarize text</p>
+        </div>
+        <div>
+          <p>Streamline</p>
+          <p>Streamline text</p>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
-
-
-
